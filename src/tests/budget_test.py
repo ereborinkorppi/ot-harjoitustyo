f68955497354751cf30746f1bc3e6373977c8a11
Eero Jantunen <jantunen.eero@gmail.com>
2021-11-20 22:37:14 +0200

    added first tests

---
+++ src/tests/budget_test.py
@@ -0,0 +1,25 @@
+import unittest
+from src.budget import Budget
+
+class TestBudget(unittest.TestCase):
+    def setUp(self):
+        self.budget = Budget()
+        
+    def test_budget_created(self):
+        self.assertNotEqual(self.budget, None)
+    
+    def test_incomes_right(self):
+        self.assertEqual(self.budget.incomes, 0)
+        
+    def test_expenses_right(self):
+        self.assertEqual(self.budget.expenses, 0)
+        
+    def test_add_income(self):
+        self.budget.add_income(300.5)
+        self.assertEqual(self.budget.incomes, 300.5)
+        
+    def test_add_expense(self):
+        self.budget.add_expense(112)
+        self.assertEqual(self.budget.expenses, 112)
+        
+    
