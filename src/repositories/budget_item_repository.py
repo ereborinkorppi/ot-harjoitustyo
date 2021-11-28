e68beecd61898a50de2b5e7893a4c57c66de1f44
Eero Jantunen <jantunen.eero@gmail.com>
2021-11-28 21:35:59 +0200

    modified code to better quality

--- src/repositories/budget_item_repository.py
+++ src/repositories/budget_item_repository.py
@@ -4,7 +4,7 @@ class BudgetItemRepository:
 
     def __init__(self, connection):
         self._connection = connection
-        
+
     def create(self, budget_item):
 
         cursor = self._connection.cursor()
@@ -26,23 +26,31 @@ class BudgetItemRepository:
         next_id = cursor.fetchone()[0]
 
         return next_id
-    
+
     def get_incomes(self):
         cursor = self._connection.cursor()
 
-        cursor.execute('select sum(sum) from budget where type = 1'              )
+        cursor.execute('select sum(sum) from budget where type = 1')
 
         incomes = cursor.fetchone()[0]
 
         return incomes
-    
+
     def get_expenses(self):
         cursor = self._connection.cursor()
 
-        cursor.execute('select sum(sum) from budget where type = 2'              )
+        cursor.execute('select sum(sum) from budget where type = 2')
 
         expenses = cursor.fetchone()[0]
 
         return expenses
+    
+    def delete_all(self):
+
+        cursor = self._connection.cursor()
+
+        cursor.execute('delete from budget')
+
+        self._connection.commit()
 
 budget_item_repository = BudgetItemRepository(get_database_connection())
--- src/services/budget_service.py
+++ src/services/budget_service.py
@@ -14,40 +14,40 @@ class BudgetService:
         self._budget_item_repository = budget_item_repository
         self.incomes = 0
         self.expenses = 0
-    
+
     def create_budget_item (self, id, sum, type):
         budget_item = self._budget_item_repository.create(BudgetItem(id, sum, type))
-        
+
         return budget_item
-    
+
     def get_next_id(self):
         next_id = self._budget_item_repository.find_next_id()
-        
+
         return next_id
-    
+
     def get_incomes(self):
         incomes = self._budget_item_repository.get_incomes()
-        if incomes == None:
+        if incomes is None:
             incomes = 0
-        
+
         return incomes
-    
+
     def get_expenses(self):
         expenses = self._budget_item_repository.get_expenses()
-        if expenses == None:
+        if expenses is None:
             expenses = 0
-        
+
         return expenses
-    
+
     def get_budget(self):
         incomes = self._budget_item_repository.get_incomes()
         expenses = self._budget_item_repository.get_expenses()
-        if incomes == None:
+        if incomes is None:
             incomes = 0
-        if expenses == None:
+        if expenses is None:
             expenses = 0
         budget = round(incomes - expenses, 2)
-        
+
         return budget
-    
+
 budget_service = BudgetService()
