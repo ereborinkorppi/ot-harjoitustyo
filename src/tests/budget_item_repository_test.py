fb80b718b4459f800524bab44c24aa68c4c4594b
Eero Jantunen <jantunen.eero@gmail.com>
2021-11-29 10:48:10 +0200

    new tests

--- src/tests/budget_item_repository_test.py
+++ src/tests/budget_item_repository_test.py
@@ -5,12 +5,24 @@ from entities.budget_item import BudgetItem
 class TestBudgetItemRepository(unittest.TestCase):
     def setUp(self):
         budget_item_repository.delete_all()
-        self.budget_item_tulo = BudgetItem(0, 500, 1)
-        self.budget_item_meno = BudgetItem(1, 200, 2)
-
+        self.budget_item_income1 = BudgetItem(0, 500, 1)
+        self.budget_item_expense1 = BudgetItem(1, 200, 2)
+        budget_item_repository.create(self.budget_item_income1)
+        budget_item_repository.create(self.budget_item_expense1)
+    
     def test_create(self):
-        budget_item_repository.create(self.budget_item_tulo)
+        next_id = budget_item_repository.find_next_id()
+        self.assertNotEqual(next_id, 0)
+
+    def test_find_next_id(self):
         next_id = budget_item_repository.find_next_id()
 
-        self.assertEqual(next_id, 1)
-        self.assertEqual(budget_item_repository.get_incomes(), 500)
+        self.assertEqual(next_id, 2)
+    
+    def test_get_incomes(self):
+        self.budget_item_income2 = BudgetItem(2, 100, 1)
+        budget_item_repository.create(self.budget_item_income2)
+        self.assertEqual(budget_item_repository.get_incomes(), 600)
+        
+    def test_get_expenses(self):
+        self.assertEqual(budget_item_repository.get_expenses(), 200)
