194896f68d1e9a6d78dc9d2e19573526c61034e4
Eero Jantunen <jantunen.eero@gmail.com>
2021-12-19 22:14:47 +0200

    added Scrollable frame library

--- src/ui/list_budget_items_view.py
+++ src/ui/list_budget_items_view.py
@@ -1,4 +1,5 @@
 from tkinter import ttk, constants
+from ScrollableFrame import *
 from services.budget_service import budget_service
 
 class ListBudgetItemsView:
@@ -17,36 +18,30 @@ class ListBudgetItemsView:
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         heading = ttk.Label(master=self._frame, text="Tulo- ja menoerittely", font="Arial 14 bold")
-        heading.grid(row=0, column=0, columnspan=2, pady=5)
         incomes = ttk.Label(master=self._frame, text="Tulot:", font="Arial 12 underline")
-        incomes.grid(row=1, column=0, pady=2)
+        self.scrollable_incomes = ScrollableFrame(self._frame, orient=0, height=200, width=400)
         incomes_list = budget_service.get_budget_items(1)
-        expenses_list = budget_service.get_budget_items(2)
-        i = 2
         for x in range(len(incomes_list)):
             desc = incomes_list[x].desc
-            amount = incomes_list[x].amount
-            income_item_desc = ttk.Label(master=self._frame, text=desc)
-            income_item_amount = ttk.Label(master=self._frame, text=amount)
-            income_item_desc.grid (row=i, column=0)
-            income_item_amount.grid (row=i, column=1)
-            i = i+1
+            amount = round(incomes_list[x].amount,2)
+            income_item = ttk.Label(self.scrollable_incomes.frame, text=str(amount)+ "   " + desc).pack(anchor=W)
         expenses = ttk.Label(master=self._frame, text="Menot:", font="Arial 12 underline")
-        expenses.grid(row=i, column=0, pady=2)
-        i = i+1
+        self.scrollable_expenses = ScrollableFrame(self._frame, orient=0, height=200, width=400)
+        expenses_list = budget_service.get_budget_items(2)
         for x in range(len(expenses_list)):
             desc = expenses_list[x].desc
-            amount = expenses_list[x].amount
-            expense_item_desc = ttk.Label(master=self._frame, text=desc)
-            expense_item_amount = ttk.Label(master=self._frame, text=amount)
-            expense_item_desc.grid (row=i, column=0)
-            expense_item_amount.grid (row=i, column=1)
-            i = i+1
-
+            amount = round(expenses_list[x].amount,2)
+            expense_item = ttk.Label(self.scrollable_expenses.frame, text=str(amount)+ "   " + desc).pack(anchor=W)
+        
         button = ttk.Button(
             master=self._frame,
             text="Takaisin",
             command=self._handle_home
         )
-
-        button.grid(row=i, column=0, columnspan=2, pady=5)
+        
+        heading.grid(row=0, column=0, pady=5, columnspan=2)
+        incomes.grid(row=1, column=0, pady=5)
+        self.scrollable_incomes.grid(row=2, column=0, padx=5, columnspan=2)
+        expenses.grid(row=3, column=0, pady=5)
+        self.scrollable_expenses.grid(row=4, column=0, padx=5, columnspan=2)
+        button.grid(row=5, column=0, pady=5, columnspan=2)
